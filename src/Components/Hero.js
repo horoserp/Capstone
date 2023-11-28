a76c8f64f6cf3f9c5482df4f35f2ad58f1968dcd
Robert Horosewski <horoserp@yahoo.com>
2023-11-27 21:11:00 -0500

    Complete Hero section content.

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,7 +1,16 @@
+import heroImage from '../Images/restauranfood.jpg'
+
 function Hero() {
     return (
         <section>
-            <p>This is the Hero section</p>
+            
+            <div>
+                <h1>Little Lemon</h1>
+                <h2>Chicago</h2>
+                <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+                <button>Reserve a Table</button>
+            </div>
+            <img src={heroImage} alt={"Waiter holding tray of food"} />
         </section>
     );
 }
