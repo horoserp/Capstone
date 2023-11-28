75dc591a3f0cb9415bfdf3a76d73b8e4dd8efe7f
Robert Horosewski <horoserp@yahoo.com>
2023-11-28 16:53:18 -0500

    Finish styling hero component.

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -6,7 +6,7 @@ function Hero() {
             <div>
                 <h1>Little Lemon</h1>
                 <h2>Chicago</h2>
-                <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+                <p className='short'>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
                 <button>Reserve a Table</button>
             </div>
             <img src={heroImage} alt={"Waiter holding tray of food"} />
