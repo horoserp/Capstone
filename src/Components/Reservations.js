edf0e0a087a799e044c0730605002437d400bdc5
Robert Horosewski <horoserp@yahoo.com>
2023-12-09 16:25:51 -0500

    Begin reservations component.

--- src/Components/Reservations.js
+++ src/Components/Reservations.js
@@ -1,6 +1,31 @@
+import restaurant from "../Images/restaurant.jpg";
+import backButton from "../Images/circle-arrow-left-solid.svg";
+
 function Reservations() {
     return (
-        <section>This is reservations</section>
+        <section>
+            <div className="primary-b">
+                <div className="container pad">
+                    <img
+                        src={restaurant}
+                        alt={"Outdoor seating at Little Lemon"}
+                        id="reserve-image"
+                    />
+                    <div style={{height: 350, color: '#F4CE14'}}>
+                        <h1 style={{}}>Reserve a Table</h1>
+                        <h2>(Step 1 of 2)</h2>
+                        <img
+                            src={backButton}
+                            alt={"Left pointing arrow"}
+                            id="icon"
+                        />
+                    </div>
+                </div>
+            </div>
+            <div>
+                date
+            </div>
+        </section>
     );
 }
 
