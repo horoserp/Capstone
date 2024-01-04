f6bf608979e0cf68067a827fd35a5d77f7fa30ee
Robert Horosewski <horoserp@yahoo.com>
2024-01-03 21:22:18 -0500

    Update: Adjust text size on Confirmation.

--- src/Components/Confirmation.js
+++ src/Components/Confirmation.js
@@ -16,13 +16,13 @@ function Confirmation() {
             <Reservations>Success!</Reservations>
             <div className="highlight-b pad">
                 <div className="container flex-dir">
-                    <h2 style={{margin: 0}}>Contratulations!</h2>
-                    <h3>Reservation Successful</h3>
-                    <p className="center">You will receive an email confirmation shortly.<br />
+                    <h1 style={{margin: 0}}>Contratulations!</h1>
+                    <h2>Reservation Successful</h2>
+                    <span className="center">You will receive an email confirmation shortly.<br />
                         If you need to make changes to your reservation, please call us directly.<br />
                         We look forward to your visit with us!
-                    </p>
-                    <button onClick={handleClick}>Explore Our Menu</button>
+                    </span>
+                    <button onClick={handleClick} style={{marginTop: 30}}>Explore Our Menu</button>
                 </div>
             </div>
         </>
