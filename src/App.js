b8e32102022e068d2911863c729684bb844e99d3
Robert Horosewski <horoserp@yahoo.com>
2024-05-04 22:38:33 -0400

    Fix: Render Homepage from App.

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/confirmation" element={<Confirmation />} />
       </Routes>
+      <Homepage/>
       <div className='primary-b'>
         <Footer/>
       </div>
