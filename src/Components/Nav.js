5d831bdfdcab73d1d823c1135099afd0a8bcf95d
Robert Horosewski <horoserp@yahoo.com>
2024-05-04 22:43:50 -0400

    Update: Temporarily disabled Order Online until page is finished.

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -9,7 +9,7 @@ function Nav() {
             <Link to="/about" onClick={() => window.scrollTo(0,0)}>About</Link>
             <Link to="/highlights" onClick={() => window.scrollTo(0,0)}>Menu</Link>
             <Link to="/reservations" onClick={() => window.scrollTo(0,0)}>Reservations</Link>
-            <Link to="/orderonline" onClick={() => window.scrollTo(0,0)}>Order Online</Link>
+            {/* <Link to="/orderonline" onClick={() => window.scrollTo(0,0)}>Order Online</Link> */}
             <Link to="login" onClick={() => window.scrollTo(0,0)}>Login</Link>
         </nav>
     );
